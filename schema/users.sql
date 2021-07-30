129e2f7c99a63217421c233c7fa3cf74d4628d88
hyun0k <hyunyoung9310@gmail.com>
2021-07-30 21:01:19 +0900

    chore:schema 수정 - 생성 날짜, 삭제 여부 컬럼 추가

--- schema/users.sql
+++ schema/users.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS users
     `password`      VARCHAR(45)     NOT NULL    COMMENT '비밀번호', 
     `name`          VARCHAR(45)     NOT NULL    COMMENT '회원 이름', 
     `email`         VARCHAR(320)    NOT NULL    COMMENT '이메일 주소', 
-    `phone_number`  VARCHAR(11)     NOT NULL    COMMENT '핸드폰 번호', 
+    `phone_number`  VARCHAR(11)     NOT NULL    COMMENT '핸드폰 번호',
+    `created_at`    DATETIME        NOT NULL    COMMENT '생성 날짜', 
+    `is_deleted`    TINYINT         NOT NULL    COMMENT '삭제 여부', 
      PRIMARY KEY (id)
 );
 
