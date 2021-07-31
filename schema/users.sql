fefc36fde3ecefc4cd2b7130bfe5d6ba99af381f
hyun0k <hyunyoung9310@gmail.com>
2021-07-31 18:08:59 +0900

    chore:db스키마 수정에 따른 스크립트 수정 - bcrypt암호화를 고려하여 password컬럼 길이를 60으로 늘림.

--- schema/users.sql
+++ schema/users.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users
 (
     `id`            INT UNSIGNED    NOT NULL    AUTO_INCREMENT, 
     `user_id`       VARCHAR(45)     NOT NULL    COMMENT '회원 아이디', 
-    `password`      VARCHAR(45)     NOT NULL    COMMENT '비밀번호', 
+    `password`      VARCHAR(60)     NOT NULL    COMMENT '비밀번호', 
     `name`          VARCHAR(45)     NOT NULL    COMMENT '회원 이름', 
     `email`         VARCHAR(320)    NOT NULL    COMMENT '이메일 주소', 
     `phone_number`  VARCHAR(11)     NOT NULL    COMMENT '핸드폰 번호',
