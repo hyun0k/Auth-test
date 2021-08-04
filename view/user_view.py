da04b4ee3622a464fea86f044d4c91efd103e116
hyun0k <hyunyoung9310@gmail.com>
2021-08-04 22:18:45 +0900

    feat:파라미터 유효성 검사 로직 구현 - jsonschema를 이용하여 요청으로 들어온 json값을 config에 미리 정의한 schema로 유효성을 검사

--- view/user_view.py
+++ view/user_view.py
@@ -1,17 +1,21 @@
 from flask       import request, jsonify
 from flask.views import MethodView
+from jsonschema  import validate, ValidationError
 
 from connection              import connect_db
 from service.user_service    import UserService
+from config                  import SIGNUP_SCHEMA
 
 class UserView(MethodView):
     def post(self):
         # 회원가입(사용자 생성)
         user_service = UserService()
-        connection = None
-        try:
-            user_info = request.get_json() # TODO - 파라미터 유효성 검사 필요. 데코레이터로 jsonschema 사용 예정.
+        connection   = None
+        user_info    = request.get_json() 
             
+        try:
+            validate(user_info, SIGNUP_SCHEMA)
+
             connection = connect_db()
             result = user_service.signup(user_info, connection)
             connection.commit()
@@ -19,10 +23,16 @@ class UserView(MethodView):
             return jsonify({"message" : "Account is created successfully.",
                             "data"    : result}), 201
         
+        except ValidationError as e:
+            raise e
+        
         except Exception as e:
             connection.rollback()
             raise e
         
+        finally:
+            if connection is not None:
+                connection.close()
     
     def get(self):
         # 회원정보 조회 (단일, 전체)
